0a4f250fcd4554addff74cb1c609ebc92334bdc4
Jordncodes <jordanwalters681@google.com>
2023-09-18 16:35:11 +0100

    Ticket 5

--- ticket-5/index.js
+++ ticket-5/index.js
@@ -6,7 +6,6 @@ const shoppingCart = [
   },
   {
     product: "Apples",
-
     price: 0.7,
     quantity: 8,
   },
@@ -19,7 +18,7 @@ const shoppingCart = [
 
 let totalCost = 0;
 
-for (let i = 0; i <= shoppingCart.length; i++) {
+for (let i = 0; i < shoppingCart.length; i++) {
   const item = shoppingCart[i];
   const costForItem = item.price * item.quantity;
   totalCost += costForItem;
@@ -31,3 +30,4 @@ const formatter = new Intl.NumberFormat("en-GB", {
 });
 
 console.log(`The total cost for this cart is ${formatter.format(totalCost)}`);
+
